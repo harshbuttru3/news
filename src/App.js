4ce4ce498926f13576cc2c5c7dc19a439c8ff8f8
Harsh <shivamkumarpaswan7488700578@gmail.com>
2023-08-21 14:05:22 +0530

    adding categories

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import './App.css';
 import NavBar from './components/NavBar';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
 
 import React, { Component } from 'react'
 import NewsComponent from './components/NewsComponent';
 
 export default class App extends Component {
-   c = "shivam";
   render() {
     return (
       <div>
-        <NavBar/>
-        <NewsComponent pageSize={9} country="in" category="technology" />
+        <Router>
+          <NavBar />
+          <Routes>
+            <Route path='/' element={<NewsComponent key="general" pageSize={9} country="in" category="general" />} />
+            <Route path='/business' element={<NewsComponent key="business" pageSize={9} country="in" category="business" />} />
+            <Route path='/entertainment' element={<NewsComponent key="entertainment" pageSize={9} country="in" category="entertainment" />} />
+            {/* <Route path='/general' element={<NewsComponent key="general" pageSize={9} country="in" category="general" />} /> */}
+            <Route path='/science' element={<NewsComponent key="science" pageSize={9} country="in" category="science" />} />
+            <Route path='/technology' element={<NewsComponent key="technology" pageSize={9} country="in" category="technology" />} />
+            <Route path='/health' element={<NewsComponent key="health" pageSize={9} country="in" category="health" />} />
+            <Route path='/sports' element={<NewsComponent key="sports" pageSize={9} country="in" category="sports" />} />
+          </Routes>
+        </Router>
       </div>
     )
   }
 }
+
 
 // apiKey = 9027f572576a4d77abbcbf55998d326c
